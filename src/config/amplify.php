2ca0031a2493bfe91e15530934dcd07df58aef24
Chimezie Enyinnaya <meziemichael@gmail.com>
2016-12-19 10:51:04 +0100

    Update and rename paystack.php to amplify.php

---
+++ src/config/amplify.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+	'amplify_mode' => env('AMPLIFY_MODE', 'test'),
+
+	'public_key' => env('AMPLIFY_PUBLIC_KEY', ''),
+
+    'secret_key' => env('AMPLIFY_SECRET_KEY', ''),
+
+    'payment_url' => env('AMPLIFY_PAYMENT_URL', ''),
+
+];
